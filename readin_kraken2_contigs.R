b7884a52fb676ea8c9ce47458d80497d42a207be
Huaiying Lin <ying.eddi2008@gmail.com>
2020-12-02 19:20:58 -0600

    more superman funcs

---
+++ readin_kraken2_contigs.R
@@ -0,0 +1,18 @@
+#readin kraken2 function
+readin_kraken2_contigs <- function(directory){
+  setwd(directory)
+  files <- dir(pattern="standard")
+  dflist <- NULL
+  for(i in 1:length(files)) {
+    print(i)
+    int <- fread(files[i]) %>%
+      mutate(filename=files[i])
+    
+    dflist[[i]] <- int
+  }
+  kraken2 <- do.call(rbind,dflist) %>%
+    select(contig=V2,taxon=V3,length=V4,filename) %>%
+    mutate(seq_id=gsub("\\.standard\\.txt","",filename),
+           cohort="biobank")
+  return(kraken2)
+}
