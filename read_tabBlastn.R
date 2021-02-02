2e2fadd75b4c6c8c2afe2a4cf2fe7b920c897b35
yingeddi2008 <ying.eddi2008@gmail.com>
2021-02-02 10:25:44 -0800

    could be used for default tabular blast output

--- read_tabBlastn.R
+++ read_tabBlastn.R
@@ -7,7 +7,9 @@ read_tabBlastn <- function(filename, ident = 85, cov = 85 ){
   CNAMES <- c("qaccver", "saccver", "pident", "length", "mismatch", "gapopen", "qstart", "qend", 
              "sstart", "send","evalue", "bitscore", "score", "qlen", "qcovus", "sstrand", "slen")
   
-  bo <- readr::read_delim(filename, delim = "\t", col_names = CNAMES) 
+  bo <- readr::read_delim(filename, delim = "\t", col_names = F) 
+  
+  colnames(bo) <- CNAMES[1:ncol(bo)]
   
   bo %>%
     mutate(qcov = (qend - qstart + 1)/qlen * 100,
