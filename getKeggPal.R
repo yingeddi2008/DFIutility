0a7fd95d413e380a414e7bf6d8a782a6d656a72e
yingeddi2008 <ying.eddi2008@gmail.com>
2021-02-17 14:11:52 -0800

    change to color by rank1

--- getKeggPal.R
+++ getKeggPal.R
@@ -1,24 +1,51 @@
-getKeggPal <- function (keggtax, rn = 3, paln = 10) {
+getKeggPal <- function (keggtax, rn = 2) {
   
   require(yingtools2)
   require(tidyverse)
+  require(assertive.types)
+  require(assertive.base)
   
-  basecols <- tibble( rank2 = c("09182 Protein families: genetic information processing",
-                                "09183 Protein families: signaling and cellular processes",
-                                "09181 Protein families: metabolism","09132 Signal transduction",
-                                "09101 Carbohydrate metabolism","09164 Neurodegenerative disease",
-                                "09191 Unclassified: metabolism","09105 Amino acid metabolism",
-                                "09102 Energy metabolism","09131 Membrane transport",
-                                "09172 Infectious disease: viral","09143 Cell growth and death",
-                                "09171 Infectious disease: bacterial","09108 Metabolism of cofactors and vitamins",
-                                "09141 Transport and catabolism","09194 Poorly characterized",
-                                "09152 Endocrine system","09145 Cellular community - prokaryotes"),
-                      col = c("#FAFD7CFF","#82491EFF","#24325FFF","#B7E4F9FF","#FB6467FF","#526E2DFF","#E762D7FF",
-                              "#E89242FF","#FAE48BFF","#A6EEE6FF","#917C5DFF","#00468BFF","#ED0000FF","#42B540FF",
-                              "#0099B4FF","#925E9FFF","#FDAF91FF","#AD002AFF")
-                        
-  ) %>%
-    dplyr::slice(1:paln)
+  rn <- assert_is_a_number(rn)
+  
+  if ( between(rn, 2,4) ){
+    warning(
+      switch(as.character(rn),
+      "2" = "Good choice of rank level at 2, and it is the default!",
+      "3" = "Rank 3 may work! Possibly a bit busy.",
+      "4" = "Too many colors! Don't recommend it!"
+      )
+    )
+  } else {
+    stop("rn argument is out of range! Please correct! It should be between 2, and 4.")
+  }
+  
+  # switch to rank2
+  # basecols <- tibble( rank2 = c("09182 Protein families: genetic information processing",
+  #                               "09183 Protein families: signaling and cellular processes",
+  #                               "09181 Protein families: metabolism","09132 Signal transduction",
+  #                               "09101 Carbohydrate metabolism","09164 Neurodegenerative disease",
+  #                               "09191 Unclassified: metabolism","09105 Amino acid metabolism",
+  #                               "09102 Energy metabolism","09131 Membrane transport",
+  #                               "09172 Infectious disease: viral","09143 Cell growth and death",
+  #                               "09171 Infectious disease: bacterial","09108 Metabolism of cofactors and vitamins",
+  #                               "09141 Transport and catabolism","09194 Poorly characterized",
+  #                               "09152 Endocrine system","09145 Cellular community - prokaryotes"),
+  #                     col = c("#FAFD7CFF","#82491EFF","#24325FFF","#B7E4F9FF","#FB6467FF","#526E2DFF","#E762D7FF",
+  #                             "#E89242FF","#FAE48BFF","#A6EEE6FF","#917C5DFF","#00468BFF","#ED0000FF","#42B540FF",
+  #                             "#0099B4FF","#925E9FFF","#FDAF91FF","#AD002AFF")
+  #                       
+  # ) %>%
+  #   dplyr::slice(1:paln)
+  
+  basecols <- tibble(rank1 = c('09180 Brite Hierarchies','09100 Metabolism',
+                      '09190 Not Included in Pathway or Brite',
+                      '09120 Genetic Information Processing',
+                      '09130 Environmental Information Processing',
+                      '09140 Cellular Processes',
+                      '09160 Human Diseases',
+                      '09150 Organismal Systems'),
+                     col = c('#1F77B4FF','#FF7F0EFF','#2CA02CFF','#D62728FF',
+                     '#9467BDFF','#8C564BFF','#E377C2FF','#17BECFFF'))
   
   ranks <- paste0("rank",c(1:rn))
   rlast <- ranks[length(ranks)]
@@ -32,7 +59,7 @@ getKeggPal <- function (keggtax, rn = 3, paln = 10) {
   # start out as all gray
   
   others <- kdict %>%
-    filter(! rank2 %in% basecols$rank2 ) 
+    filter(! rank1 %in% basecols$rank1 ) 
   
   greyN <- others %>%
     nrow()
@@ -42,8 +69,8 @@ getKeggPal <- function (keggtax, rn = 3, paln = 10) {
     mutate(greycol = greycols)
   
   kpal <- kdict %>%
-    left_join(basecols, by = c("rank2")) %>%
-    group_by(rank2, col) %>%
+    left_join(basecols, by = c("rank1")) %>%
+    group_by(rank1, col) %>%
     summarise(catN = n(),
               !!rlast := list(.data[[rlast]])) %>%
     mutate(cols = ifelse(is.na(col),
