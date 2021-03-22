730c2173aa0c7c75c97698eca4e3b6cb5c735854
yingeddi2008 <ying.eddi2008@gmail.com>
2021-03-22 16:45:32 -0500

    source from multiple places

--- getKraken2std.R
+++ getKraken2std.R
@@ -1,6 +1,7 @@
 argopts <- commandArgs(trailingOnly = TRUE) 
 path = argopts[1]
 
-source("~/Documents/Eddi/DFIutility/readin_kraken2_standard.R")
+try(source("~/Documents/Eddi/DFIutility/readin_kraken2_standard.R"))
+try(source("~/OneDrive - The University of Chicago/DFIutility/readin_kraken2_standard.R"))
 kall = readin_kraken2_standard(path, recursive = F, saveIntermediate = F)
 saveRDS(kall, "all.kraken2.standard.rds")
