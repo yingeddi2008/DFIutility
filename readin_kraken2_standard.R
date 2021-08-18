d6f52ebcf5640c89dd6894d64e05b0d8398f2798
yingeddi2008 <ying.eddi2008@gmail.com>
2021-08-18 11:41:15 -0500

    sys.glob in place of dir

--- readin_kraken2_standard.R
+++ readin_kraken2_standard.R
@@ -5,15 +5,16 @@ readin_kraken2_standard <- function(directory, recursive = T, saveIntermediate =
   
   print(paste0("Looking for kraken2 standard output to load..."))
   
-  files <- dir(path = directory, pattern="standard", recursive = recursive)
+  files <- Sys.glob(file.path(directory, "*standard.txt"))
+  
   dflist <- NULL
   
   print(paste0("Found ", length(files), " files to load..."))
   
   for(i in 1:length(files)) {
-    print(paste0("  >> loading ... ", file.path(directory,files[i])))
+    print(paste0("  >> loading ... ", files[i]))
     
-    dflist[[i]] <- fread(file.path(directory,files[i]), header = F,
+    dflist[[i]] <- fread(files[i], header = F,
                          col.names = c("uc","read","taxon","length","LCAmap")) %>%
       mutate(filename=files[i]) %>%
       dplyr::count(filename, uc, taxon, name = "numseqs", sort = T)
@@ -25,7 +26,8 @@ readin_kraken2_standard <- function(directory, recursive = T, saveIntermediate =
   }
   
   kraken2 <- do.call(rbind,dflist) %>%
-    mutate(seq_id=gsub("\\.standard\\.txt","",filename),
+    mutate(filename = sapply(str_split(filename,"/"), function(x) x[length(x)]),
+           seq_id=gsub("\\.standard\\.txt","",filename),
            taxidex = str_extract(taxon,"taxid [0-9]+")) %>%
     separate(taxidex, c("taxfoo","taxid"), convert = T) %>%
     select(seq_id, uc, taxon, taxid, numseqs, filename)
