4bbacc24b0f621b08f7e0f26db8170d4f6a03ab2
yingeddi2008 <ying.eddi2008@gmail.com>
2021-08-18 14:28:10 -0500

    sys.glob

--- getKraken2std.R
+++ getKraken2std.R
@@ -3,5 +3,5 @@ path = argopts[1]
 
 try(source("~/Documents/Eddi/DFIutility/readin_kraken2_standard.R"))
 try(source("~/OneDrive - The University of Chicago/DFIutility/readin_kraken2_standard.R"))
-kall = readin_kraken2_standard(path, recursive = F, saveIntermediate = F)
+kall = readin_kraken2_standard(path, saveIntermediate = F)
 saveRDS(kall, "all.kraken2.standard.rds")
