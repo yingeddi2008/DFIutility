8be72caa64c0f57a17c4afe774203e34c002a4d0
yingeddi2008 <ying.eddi2008@gmail.com>
2022-03-10 11:00:28 -0800

    add ungroup

--- getRdpPal.R
+++ getRdpPal.R
@@ -1,43 +1,48 @@
 getShades <- function(spdf){
-  
+
   hexcol <- unique(spdf$color)
-  
+
   if (nrow(spdf) > 3){
     resdf <- spdf %>%
-      mutate(cols = rep(shades(hexcol, variation = 0.25), 
-                        length.out = nrow(spdf) 
+      mutate(cols = rep(shades(hexcol, variation = 0.25),
+                        length.out = nrow(spdf)
       )
       )
   } else {
     resdf <- spdf %>%
       mutate(cols = shades(hexcol, variation = 0.25, ncolor = nrow(spdf)))
   }
-  
+
   return(resdf)
 }
 
 getRdpPal <- function(tax) {
+
   require(tidyverse)
   require(yingtools2)
+
+  tax <- tax %>%
+    ungroup()
+
   if (class(tax)[1] %in% c("phyloseq", "taxonomyTable")) {
     tax <- get.tax(tax.obj)
   }
-  ranks <- c("Kingdom", "Phylum", "Class", "Order", "Family", 
+  ranks <- c("Kingdom", "Phylum", "Class", "Order", "Family",
              "Genus")
-  
+
   if (!all(ranks %in% names(tax))) {
     stop("Error: need to have taxon levels: Kingdom, Phylum, Class, Order, Family, Genus, Species")
   }
-  
+
   tax.dict <- tax %>%
-    select(all_of(ranks)) %>% 
+    select(all_of(ranks)) %>%
     distinct()
-  
+
   # set all color to gray as base
-  tax.dict <- tax.dict %>% 
-    mutate(color = rep(shades("gray", variation = 0.25), 
+  tax.dict <- tax.dict %>%
+    mutate(color = rep(shades("gray", variation = 0.25),
                        length.out = nrow(tax.dict)))
-  
+
 # color for each level ------------------------------------------------------------
   phypal <- tibble(Phylum = c("Proteobacteria","Actinobacteria","Bacteroidetes"),
                    phycol = c("red","#A77097","#51AB9B"))
@@ -49,7 +54,7 @@ getRdpPal <- function(tax) {
   genpal <- tibble(Genus = c("Enterococcus","Streptococcus","Staphylococcus",
                              "Lactobacillus"),
                    gencol = c("#129246","#9FB846","#f1eb25", "#3b51a3"))
-  
+
   tax.split <- tax.dict %>%
     left_join(phypal) %>%
     left_join(ordpal) %>%
@@ -67,7 +72,7 @@ getRdpPal <- function(tax) {
       !is.na(famcol) ~ famcol,
       !is.na(ordcol) ~ ordcol,
       !is.na(phycol) ~ phycol,
-      TRUE ~ color) 
+      TRUE ~ color)
       ) %>%
     select(Kingdom:Genus, color) %>%
     group_split(color)
