644979bdd742235900b7204d30ef24d5a5b2d136
yingeddi2008 <ying.eddi2008@gmail.com>
2025-04-16 09:59:54 -0700

    updated 16s

--- getRdpPal.R
+++ getRdpPal.R
@@ -53,17 +53,22 @@ getRdpPal <- function(tax) {
                               "Bacteroidota"),
                    phycol = c("red","red","red",
                               "#A77097","#A77097","#51AB9B","#51AB9B"))
-  ordpal <- tibble(Order = c("Clostridiales"),
-                   ordcol = c("#9C854E"))
-  fampal <- tibble(Family = c("Lachnospiraceae","Ruminococcaceae","Oscillospiraceae","Erysipelotrichaceae",
-                              "Lactobacillaceae"),
-                   famcol = c("#EC9B96","#9AAE73","#9AAE73","orange","#3b51a3"))
+  
+  clapal <- tibble(Class = c("Clostridia"),
+                   clacol = c("#9C854E"))
+  
+  ordpal <- tibble(Order = c("Clostridiales", "Eubacteriales"),
+                   ordcol = c("#9C854E", "#9C854E"))
+
+  fampal <- tibble(Family = c("Lachnospiraceae","Ruminococcaceae","Oscillospiraceae","Erysipelotrichaceae", "Lactobacillaceae", "Staphylococcaceae"),
+                   famcol = c("#EC9B96","#9AAE73","#9AAE73","orange","#3b51a3", "#f1eb25"))
   genpal <- tibble(Genus = c("Enterococcus","Streptococcus","Staphylococcus",
-                             "Lactobacillus"),
-                   gencol = c("#129246","#9FB846","#f1eb25", "#3b51a3"))
+                             "Lactobacillus", "Turicibacter", "Akkermansia"),
+                   gencol = c("#129246","#9FB846","#f1eb25", "#3b51a3", "#bf5700", "#63615f"))
 
   tax.split <- tax.dict %>%
     left_join(phypal) %>%
+    left_join(clapal) %>% 
     left_join(ordpal) %>%
     left_join(fampal) %>%
     # ambiguous genus match
@@ -78,6 +83,7 @@ getRdpPal <- function(tax) {
       !is.na(gencol) ~ gencol,
       !is.na(famcol) ~ famcol,
       !is.na(ordcol) ~ ordcol,
+      !is.na(clacol) ~ clacol,
       !is.na(phycol) ~ phycol,
       TRUE ~ color)
       ) %>%
